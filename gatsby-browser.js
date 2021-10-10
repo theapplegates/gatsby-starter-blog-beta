ac2994ede5ff3b5d433d248660f1cf84e14face4
Paul Applegate <paul@theapplegates.com>
2021-10-10 14:23:54 +0000

    whatever

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
@@ -10,6 +16,6 @@ import "./src/style.css"
 import "prismjs/themes/prism.css"
 
 const styles = require("@fortawesome/fontawesome-svg-core/styles.css")
-const config = require("@fortawesome/fontawesome-svg-core")
+// const config = require("@fortawesome/fontawesome-svg-core")
 
 config.autoAddCss = false
